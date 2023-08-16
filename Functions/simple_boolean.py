40855a4a63cf0c149853f41ec23f4971e3b5ac0e
Coimbra <adsjkbd@gmail.com>
2023-08-16 09:26:22 -0300

    Exercícios functions

---
+++ Functions/simple_boolean.py
@@ -0,0 +1,13 @@
+'''
+Define a function called myfunc that takes in a Boolean value (True or False). 
+If True, return 'Hello', and if False, return 'Goodbye'
+'''
+
+def myfunc(msg):
+    if msg == True:
+        return 'Hello'
+    elif msg == False:
+        return 'Goodbye'
+
+print(myfunc(True))          
+        
---
+++ Functions/using_booleans.py
@@ -0,0 +1,13 @@
+'''
+Define a function called myfunc that takes three arguments, x, y and z.
+If z is True, return x.  If z is False, return y.
+'''
+
+def myfunc(x,y,z):
+    if z == True:
+        return x
+    else:
+        return y
+
+print(myfunc('Hello','Goodbye',False))          
+        
